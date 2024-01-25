cd9f66ac41866ca2f66d17f6fbb26b608a4bb878
Bao <20127557@student.hcmus.edu.vn>
2024-01-25 21:00:47 +0700

    BL: test

--- Frontend/user/src/pages/Home/index.tsx
+++ Frontend/user/src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
-import { TestTitle } from './components'
+import { TestTitle } from './components';
 
 export function HomePage() {
   return (
     <>
-      <div className='mx-auto p-4 text-center text-3xl font-bold uppercase text-pink-950'>Home</div>
+      <div className='mx-auto p-4 text-center text-3xl font-bold uppercase text-pink-950'>HomePage</div>
       <TestTitle title='test' />
     </>
   )
