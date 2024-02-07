8998099d42c63334063c210412bd67e382a949f4
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-02-07 20:08:37 +0700

    feat: create card list

--- packages/shared/src/api/CardlistApi.ts
+++ packages/shared/src/api/CardlistApi.ts
@@ -1,27 +1,40 @@
 import { z } from "zod";
-import { CardlistSchema } from "../schemas";
+import { CardlistSchema } from "../schemas/CardList";
 
-export const CreateCardlistRequestSchema = CardlistSchema.CardlistSchema.omit({
+// export const CreateCardlistRequestSchema = CardlistSchema.CardlistSchema.omit({
+//   _id: true,
+//   cards: true,
+//   watcher_email: true,
+// });
+export const CreateCardlistRequestSchema = CardlistSchema.omit({
   _id: true,
+  board_id: true,
+  name: true,
   cards: true,
   watcher_email: true,
+  archive_at: true,
+}).merge(
+  z.object({
+    index: z.number().default(0),
+  })
+);
+
+export type CreateCardlistRequest = z.infer<typeof CreateCardlistRequestSchema>;
+
+export const GetallCardlistByBoardIdRequestSchema = CardlistSchema.omit({
+  board_id: true,
 });
-export const GetallCardlistByBoardIdRequestSchema =
-  CardlistSchema.CardlistSchema.omit({
-    board_id: true,
-  });
 export const CreateCardlistResponseSchema = z.object({
-  data: CardlistSchema.CardlistSchema,
+  data: CardlistSchema,
 });
 export const GetallCardlistResponseSchema = z.object({
-  data: CardlistSchema.CardlistSchema.array(),
+  data: CardlistSchema.array(),
 });
 
 export const GetallCardlistByBoardIdResponseSchema = z.object({
-  data: CardlistSchema.CardlistSchema.array(),
+  data: CardlistSchema.array(),
 });
 
-export type CreateCardlistRequest = z.infer<typeof CreateCardlistRequestSchema>;
 export type GetallCardlistByBoardIdRequest = z.infer<
   typeof GetallCardlistByBoardIdRequestSchema
 >;
