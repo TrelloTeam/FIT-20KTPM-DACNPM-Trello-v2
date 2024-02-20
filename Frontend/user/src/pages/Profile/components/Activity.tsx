243e39f2a5a3c75e6e5af8d149d2761d3e39709a
TinInvisible <botomtinlon@gmail.com>
2024-02-20 14:56:24 +0700

    update layout

--- Frontend/user/src/pages/Profile/components/Activity.tsx
+++ Frontend/user/src/pages/Profile/components/Activity.tsx
@@ -29,12 +29,11 @@ export const ActivityComponent: React.FC = () => {
     <div className='mx-64 mt-4 max-w-2xl bg-white p-8'>
       <div className={`mb-10`}>
         <div className={'flex flex-row items-center space-x-4 border-b-2 border-gray-300 pb-2'}>
-          <SlPeople />
+          <SlPeople size={'20px'}/>
           <p className={`my-2  text-lg font-semibold text-gray-700`}>Workspace</p>
         </div>
 
         {workspace?.map((w, index) => (
-
           <div className='my-2 flex flex-row items-center space-x-4 border-b-2 border-gray-300 pb-2'>
             <p className={`ml-2 font-light text-gray-700`}>{w.name}</p>
 
@@ -47,28 +46,29 @@ export const ActivityComponent: React.FC = () => {
         ))}
       </div>
       <div className={`mb-10`}>
-        <div className={'flex flex-row border-b-2 border-gray-300  items-center space-x-4 border-b-2 border-gray-300 pb-2'}>
-          <RxActivityLog />
+        <div
+          className={'flex flex-row items-center space-x-4  border-b-2 border-b-2 border-gray-300 border-gray-300 pb-2'}
+        >
+          <RxActivityLog size={'20px'}/>
           <p className={`my-2  text-lg font-semibold text-gray-700`}>Activity</p>
         </div>
         {activity?.map((a, index) => (
-
           <div key={index} className='my-2 space-x-4 border-b-2 border-gray-300 pb-2'>
             <div className={`flex flex-row items-center space-x-2`}>
               <img
                 src={avtPath} // Replace with your avatar image source
                 alt='Avatar'
-                className='h-10 w-10 rounded-full border'
+                className='h-6 w-6 rounded-full border'
               />
-              <p className={`text-gray-700`}>{a.content}</p>
-            </div>
-
-
-            <div className={`flex flex-row items-center space-x-2`}>
-              <p className={`text-sm font-light text-gray-700 `}>Jan 29 at 9.00 AM. On board </p>
-              <p className={`flex flex-row items-center font-semibold`}>
-                My Board <MdOutlineLock className='ml-2 text-red-500' size={'15px'} />
-              </p>
+              <div>
+                <p className={`text-gray-700`}>{a.content}</p>
+                <div className={`flex flex-row items-center space-x-2`}>
+                  <p className={`text-sm font-light text-gray-700 `}>Jan 29 at 9.00 AM. On board </p>
+                  <p className={`flex flex-row items-center font-semibold`}>
+                    My Board <MdOutlineLock className='ml-2 text-red-500' size={'15px'} />
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
