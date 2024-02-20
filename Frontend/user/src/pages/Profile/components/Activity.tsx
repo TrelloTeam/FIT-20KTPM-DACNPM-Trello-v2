e100d000c2b5edb9c242d047eddc3e84dac7ca34
TinInvisible <botomtinlon@gmail.com>
2024-02-20 13:09:49 +0700

    update

--- Frontend/user/src/pages/Profile/components/Activity.tsx
+++ Frontend/user/src/pages/Profile/components/Activity.tsx
@@ -2,7 +2,9 @@
 import React, { useEffect, useState } from 'react'
 import { Activity, Workspace } from '../type'
 import { activityData, workspaceData } from '../testData'
-import { MdOutlineLock, MdOutlineLockOpen  } from "react-icons/md";
+import { MdOutlineLock, MdOutlineLockOpen } from 'react-icons/md'
+import { RxActivityLog } from 'react-icons/rx'
+import { SlPeople } from 'react-icons/sl'
 export const ActivityComponent: React.FC = () => {
   const [activity, setActivity] = useState<Activity[]>()
   const [workspace, setWorkspace] = useState<Workspace[]>()
@@ -23,23 +25,34 @@ export const ActivityComponent: React.FC = () => {
   return (
     <div className='mx-64 mt-4 max-w-2xl bg-white p-8'>
       <div className={`mb-10`}>
-        <p className={`my-2 border-b-2 border-gray-300 pb-2 text-lg font-semibold text-gray-700`}>Workspace</p>
+        <div className={'flex flex-row items-center space-x-4 border-b-2 border-gray-300 pb-2'}>
+          <SlPeople />
+          <p className={`my-2  text-lg font-semibold text-gray-700`}>Workspace</p>
+        </div>
+
         {workspace?.map((w, index) => (
-          <div className='my-2 ml-2 flex flex-row items-center space-x-4 border-b-2 border-gray-300 pb-2'>
-            <p className={`font-light text-gray-700`}>{w.name}</p>
-            {w.visibility === 'public' ? <MdOutlineLockOpen className='text-green-500' /> : <MdOutlineLock className='text-red-500' />}
+          <div className='my-2 flex flex-row items-center space-x-4 border-b-2 border-gray-300 pb-2'>
+            <p className={`ml-2 font-light text-gray-700`}>{w.name}</p>
+            {w.visibility === 'public' ? (
+              <MdOutlineLockOpen className='text-green-500' />
+            ) : (
+              <MdOutlineLock className='text-red-500' />
+            )}
           </div>
         ))}
       </div>
       <div className={`mb-10`}>
-        <p className={`my-2 border-b-2 border-gray-300 pb-2 text-lg font-semibold text-gray-700`}>Activity</p>
+        <div className={'flex flex-row border-b-2 border-gray-300  items-center space-x-4 border-b-2 border-gray-300 pb-2'}>
+          <RxActivityLog />
+          <p className={`my-2  text-lg font-semibold text-gray-700`}>Activity</p>
+        </div>
         {activity?.map((a, index) => (
-          <div className='my-2 space-x-4 border-b-2 border-gray-300 pb-2'>
+          <div className='my-2 space-x-4  pb-2'>
             <p className={`text-gray-700`}>{a.content}</p>
-            <div className={`flex flex-row space-x-2 items-center`}>
-              <p className={`text-sm font-light text-gray-700 `}>Jan 29 at 9.00 AM. On board  </p>
-              <p className={`font-semibold flex flex-row items-center`}> 
-              My Board <MdOutlineLock className='text-red-500 ml-2' size={'15px'} />
+            <div className={`flex flex-row items-center space-x-2`}>
+              <p className={`text-sm font-light text-gray-700 `}>Jan 29 at 9.00 AM. On board </p>
+              <p className={`flex flex-row items-center font-semibold`}>
+                My Board <MdOutlineLock className='ml-2 text-red-500' size={'15px'} />
               </p>
             </div>
           </div>
