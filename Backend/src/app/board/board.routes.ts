05fb8d535bb1e76845337ac7f4c39a3b091a3ee6
Phuoc Dinh <phuocdinh462@gmail.com>
2024-02-22 18:49:07 +0700

    feat: IdValidationPipe

--- Backend/src/app/board/board.routes.ts
+++ Backend/src/app/board/board.routes.ts
@@ -2,12 +2,6 @@ import { IRouteParams } from '@/decorators'
 import { RequestMethod } from '@nestjs/common'
 
 export const BoardRoutes = {
-  // getAllBoard: {
-  //   path: '/api/board',
-  //   method: RequestMethod.GET,
-  //   jwtSecure: false
-  // } as IRouteParams,
-
   getBoardsByWorkspaceId: {
     path: '/api/board/getBoardsByWorkspaceId/:workspace_id',
     method: RequestMethod.GET,
--- Backend/src/pipes/index.ts
+++ Backend/src/pipes/index.ts
@@ -1,1 +1,2 @@
 export * from './ZodValidationPipe'
+export * from './idValidationPipe'
