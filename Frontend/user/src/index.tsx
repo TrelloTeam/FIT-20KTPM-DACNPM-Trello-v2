4812de04cbd5c17655ca00dc2d6a0cb9ce0e92d6
Tran Huu Chinh <74779503+tranhuuchinh@users.noreply.github.com>
2024-02-24 23:03:04 +0700

    Revert "HC: update header v1"

--- Frontend/user/src/index.tsx
+++ Frontend/user/src/index.tsx
@@ -2,11 +2,11 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/index.tsx'
+
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
