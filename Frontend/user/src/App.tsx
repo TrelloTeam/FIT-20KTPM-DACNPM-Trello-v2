ab3806bc9def7a15eab01106944261d585df5ffd
TinInvisible <botomtinlon@gmail.com>
2024-03-06 10:55:21 +0700

    update

--- Frontend/user/src/App.tsx
+++ Frontend/user/src/App.tsx
@@ -2,14 +2,13 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { Outlet } from 'react-router'
 import Header from './components/Header'
+import { AccountManagement } from './pages'
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <div style={{ paddingTop: '50px' }}>
-        <Outlet />
-      </div>
+      {/* <Header /> */}
+      <AccountManagement/>
     </Provider>
   )
 }
