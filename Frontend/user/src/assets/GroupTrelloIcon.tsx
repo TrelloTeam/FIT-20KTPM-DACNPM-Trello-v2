440e8e99794f9a3a406c16218c55890398ae0aba
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-03-09 15:20:33 +0700

    TK: completed UI BoardBar-detail

---
+++ Frontend/user/src/assets/GroupTrelloIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Icon } from '@mui/material'
+import YourLogo from '~/assets/GroupTrelloIcon.svg'
+
+export const GroupTrelloIcon = () => (
+    <Icon>
+        <img src={YourImage}/>
+    </Icon>
+)
