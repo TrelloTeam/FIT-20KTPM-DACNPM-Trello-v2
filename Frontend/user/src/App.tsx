d7951ed638f9f85fb642c3195eadd2e5d26ae63f
TinInvisible <botomtinlon@gmail.com>
2024-03-09 15:52:17 +0700

    fix App.tsx

--- Frontend/user/src/App.tsx
+++ Frontend/user/src/App.tsx
@@ -2,9 +2,6 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { Outlet } from 'react-router'
 import Header from './components/Header'
-
-import { AccountManagement } from './pages'
-
 import { ThemeProvider } from './components/Theme/themeContext'
 
 function App() {
@@ -13,7 +10,7 @@ function App() {
       <ThemeProvider>
         <Header />
         <div style={{ paddingTop: '50px' }}>
-          <AccountManagement page='profile' />
+          <Outlet />
         </div>
       </ThemeProvider>
     </Provider>
