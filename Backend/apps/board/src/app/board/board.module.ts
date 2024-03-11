30e4709b851347092f40bb89ca6da340276cefcf
Bao <20127557@student.hcmus.edu.vn>
2024-03-11 22:03:26 +0700

    fix: rename ms controller

--- Backend/apps/board/src/app/board/board.module.ts
+++ Backend/apps/board/src/app/board/board.module.ts
@@ -2,7 +2,7 @@ import { BoardMModule } from '@app/common/database/modules'
 import { Module } from '@nestjs/common'
 
 import { BoardController } from './controllers/board.controller'
-import { BoardMSController } from './controllers/board.ms.controller '
+import { BoardMSController } from './controllers/board.ms.controller'
 import { BoardService } from './services/board.service'
 
 @Module({
