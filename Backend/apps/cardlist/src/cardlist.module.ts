3392de335fbd7c4df105cafe92c89d2c8fcfa733
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-03-16 14:48:30 +0700

    feat: add auth in route

--- Backend/apps/cardlist/src/cardlist.module.ts
+++ Backend/apps/cardlist/src/cardlist.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import * as Joi from 'joi'
+import { AuthModule } from '@app/common/auth/auth.module'
+import { KeycloakConfigService } from '@app/common/auth/auth.service'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
 import { configuration } from '@app/common/config'
 import { CardlistModule } from './app/cardlist/cardlist.module'
 import { CardModule } from './app/card/card.module'
-
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest-keycloak-connect'
 const EnvSchema = {
   PORT: Joi.number(),
   JWT_SECRET: Joi.string().required(),
@@ -19,11 +22,28 @@ const EnvSchema = {
       validationSchema: Joi.object().keys(EnvSchema),
       load: [configuration],
     }),
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakConfigService,
+      imports: [AuthModule],
+    }),
     MongooseModule.forRoot(process.env.DB_CONN_STR || ''),
     CardlistModule,
     CardModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class CardlistServiceModule {}
