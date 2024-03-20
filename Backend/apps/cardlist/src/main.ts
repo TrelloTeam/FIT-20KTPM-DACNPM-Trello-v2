2546295db9c44e32ea0d6233ec597c2e81da9441
Lamlance <lamhoangdien113@gmail.com>
2024-03-20 20:56:37 +0700

    delete demo

--- Backend/apps/cardlist/src/main.ts
+++ Backend/apps/cardlist/src/main.ts
@@ -9,15 +9,6 @@ import { initProtos, initSwagger } from '@app/common'
 
 import { z } from 'zod'
 
-const FeatureDataSchema = z.discriminatedUnion('type', [
-  DbSchemas.FeatureSchema.FeatureLabelSchema.omit({ _id: true }),
-  DbSchemas.FeatureSchema.FeatureChecklistSchema.omit({ _id: true }),
-  DbSchemas.FeatureSchema.FeatureDateSchema.omit({ _id: true }),
-  DbSchemas.FeatureSchema.FeatureAttachmentSchema.omit({ _id: true }),
-])
-
-console.log(FeatureDataSchema.array().parse([{ type: 'attachment', link: 'abc.com', _id: '123' }]))
-
 export default async function bootstrap() {
   const GRPC_HOST = process.env.GRPC_HOST || 'localhost'
   const PORT = process.env.PORT || '3000'
