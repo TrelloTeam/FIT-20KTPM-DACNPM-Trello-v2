eb1f08c2c643567a6d0fc88e923d750b11545e42
Lamlance <lamhoangdien113@gmail.com>
2024-03-20 20:56:12 +0700

    duplicate cardlist response schema

--- Backend/apps/cardlist/src/main.ts
+++ Backend/apps/cardlist/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core'
 import { CardlistServiceModule } from './cardlist.module'
 import { join, resolve } from 'path'
 import { readdirSync } from 'fs'
-import { TrelloApi } from '@trello-v2/shared'
+import { TrelloApi, DbSchemas } from '@trello-v2/shared'
 
 import 'dotenv/config'
 import { initProtos, initSwagger } from '@app/common'
+
+import { z } from 'zod'
+
+const FeatureDataSchema = z.discriminatedUnion('type', [
+  DbSchemas.FeatureSchema.FeatureLabelSchema.omit({ _id: true }),
+  DbSchemas.FeatureSchema.FeatureChecklistSchema.omit({ _id: true }),
+  DbSchemas.FeatureSchema.FeatureDateSchema.omit({ _id: true }),
+  DbSchemas.FeatureSchema.FeatureAttachmentSchema.omit({ _id: true }),
+])
+
+console.log(FeatureDataSchema.array().parse([{ type: 'attachment', link: 'abc.com', _id: '123' }]))
 
 export default async function bootstrap() {
   const GRPC_HOST = process.env.GRPC_HOST || 'localhost'
